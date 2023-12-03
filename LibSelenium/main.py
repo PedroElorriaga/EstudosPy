3104e44407db3c9ba7beaa17bf6d69b9cde5e24c
Elorriaga <pedro.elorriaga@accenture.com>
2023-12-03 19:35:30 -0300

    Envio 03/12/23

--- LibSelenium/main.py
+++ LibSelenium/main.py
@@ -23,32 +23,17 @@ def make_chrome_browse(*optionsParam):
 
 if __name__ == '__main__':
     
-    user_data_path = r'C:\Users\Pedro\AppData\Local\Google\Chrome\User Data'
-    options_tuple = '--no-sandbox', '--start-maximized', f'--user-data-dir={user_data_path}',
+    errors = []
+    options_tuple = '--start-maximized', '--disable-gpu',
     browser = make_chrome_browse(*options_tuple)
 
     browser.get('https://www.google.com.br')
 
     try:
-        # PESQUISAR POR CORINTHIANS
+        # PEQUISAR POR X
         element_search = WebDriverWait(browser, 4).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'gLFyf'))
         ) # UTILIZADO QUANDO TEMOS QUE ESPERAR ALGUNS ELEMENTOS JS CARREGAR
-        element_search.send_keys('Corinthians')
-        element_search.send_keys(Keys.ENTER)
-
-        # CLICAR NO PROXIMO JOGO
-        element_next_game = browser.find_element(By.CLASS_NAME, 'abhAW')
-        element_next_game.click()
-
-        # CLICAR PARA SAIR DO JOGO
-        element_exit_game = WebDriverWait(browser, 6).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="liveresults-sports-immersive__match-fullpage"]/div/div[2]/div[1]/div/div[1]'))
-        )
-        element_exit_game.click()
-        time.sleep(1.5)
-
-        # PEQUISAR POR X
         element_search = browser.find_element(By.XPATH, '//*[@id="APjFqb"]')
         element_search.click()
         element_search.send_keys(Keys.CONTROL, 'a')
@@ -59,12 +44,31 @@ if __name__ == '__main__':
         element_listar_links = browser.find_element(By.ID, 'search')
         links = element_listar_links.find_elements(By.TAG_NAME, 'a')
         links[0].click()
-        
-    except:
-        print('Não encontrado')
-        time.sleep(3)
-        browser.quit()
 
+        # CLICAR PARA FAZER LOGIN
+        element_entrar = WebDriverWait(browser, 4).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'r-2o02ov'))
+        )
+        links = element_entrar.find_elements(By.TAG_NAME, 'a')
+        links[0].click()
+        
+
+        element_email_x = WebDriverWait(browser, 4).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'r-1f1sjgu'))
+        )
+        link = element_email_x.find_elements(By.TAG_NAME, 'div')
+
+
+    except:
+        errors.append('Não encontrado')
+        browser.quit()
+        
     finally:
+        if not errors:
+            print('Deu tudo certo')
+
+        else:
+            print(errors)
+
         time.sleep(5)
     
