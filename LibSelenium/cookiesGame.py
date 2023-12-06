58c40e91066c75508093d42b21db904eaf4ecdf7
Elorriaga <pedro.elorriaga@accenture.com>
2023-12-06 10:47:06 -0300

    Envio 06/12/23

--- LibSelenium/cookiesGame.py
+++ LibSelenium/cookiesGame.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+from colorama import Fore
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -29,9 +30,19 @@ class Game():
 
         return driver
     
-    def get_website_game(self):
+    def start_game(self):
         self.browser.get('https://orteil.dashnet.org/cookieclicker/')
-        
+        self.browser.implicitly_wait(4)
+
+        print(f'Options: ')
+        for i in self.options_tuple:
+            print(Fore.CYAN + i)
+
+        print(Fore.GREEN + 'Inicializando o game...' + Fore.RESET)
+
+        self.get_website_game()
+    
+    def get_website_game(self):
         accept_cookie = WebDriverWait(self.browser, 10).until(
             EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div/a[1]'))
         )
@@ -47,25 +58,28 @@ class Game():
     def get_cookies_points(self):
         self.browser.implicitly_wait(5)
 
-        get_cookie_object = self.browser.find_element(By.ID, 'bigCookie')
+        get_cookie_image = self.browser.find_element(By.ID, 'bigCookie')
         get_cookie_count = self.browser.find_element(By.ID, 'cookies')
-        itens_store = [self.browser.find_element(By.ID, 'productPrice' + str(i)) for i in range(1,-1,-1)]
+        itens_store = [self.browser.find_element(By.ID, 'productPrice' + str(i)) for i in range(1,-1,-1)] # OUTPUT [productPrice1, productPrice0]
 
         action_keys = ActionChains(self.browser)
-        for i in range(500):
-            action_keys.click(get_cookie_object)
+        for i in range(5000):
+            action_keys.click(get_cookie_image)
             action_keys.perform()
             count = get_cookie_count.text
             for item in itens_store:
-                value = int(item.text)
-                if value <= int(count.split()[0]):
-                    upgrade_store = ActionChains(self.browser)
-                    upgrade_store.move_to_element(item)
-                    upgrade_store.click()
-                    upgrade_store.perform()
+                value = item.text
+                if int(value) <= int(count.split()[0]):
+                    self.buy_new_itens_store(item)
+
+    def buy_new_itens_store(self, item):
+        upgrade_store = ActionChains(self.browser)
+        upgrade_store.move_to_element(item)
+        upgrade_store.click()
+        upgrade_store.perform()
 
 
 
 if __name__ == '__main__':
     game = Game()
-    game.get_website_game()
+    game.start_game()
