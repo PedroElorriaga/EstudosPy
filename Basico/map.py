25a0dbbe8b5c4d37a9a44749a1f6fb7bff3d07fb
Elorriaga <pedro.elorriaga@accenture.com>
2023-12-09 18:26:37 -0300

    Envio 09/12/23

--- Basico/map.py
+++ Basico/map.py
@@ -1,4 +1,4 @@
 arr = ['Mercedes', 'BMW', 'Range Rover', 'Porsche']
 
 arr_format = list(map(lambda x: x+'-Carro',arr))
-print(arr_format)
+print(arr_format) # OUTPUT ['Mercedes-Carro', 'BMW-Carro', 'Range Rover-Carro', 'Porsche-Carro']
---
+++ LibSelenium/Teste_Case/main_test.py
@@ -0,0 +1,31 @@
+import unittest
+from selenium import webdriver
+from dataclasses import is_dataclass
+from dataClass import Product
+
+
+class ProductTestes(unittest.TestCase):
+
+    def setUp(self):
+        self.product = Product(5, 'Amendoim', 2)
+        return print('Executando testes')
+
+    def test_if_product_is_dataclass(self):
+        self.assertTrue(is_dataclass(Product))
+
+    def test_constructor(self):
+        self.assertEqual(self.product.preco, 5)
+        self.assertEqual(self.product.nome, 'Amendoim')
+        self.assertEqual(self.product.quantidade, 2)
+
+    def test_add_to_cart(self):
+        self.assertTrue(self.product.acrescentar_produto_no_carrinho())
+
+    def test_take_discount(self):
+        self.product.aplcar_desconto(1, 50)
+        self.assertEqual(self.product.preco, 2.50)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
