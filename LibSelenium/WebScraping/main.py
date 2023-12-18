70ea17a90b176a9139ea9e9b7b0b81988dceeff3
Elorriaga <pedro.elorriaga@accenture.com>
2023-12-18 16:32:09 -0300

    Envio 18/12/23

---
+++ LibSelenium/WebScraping/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(options=options)
+
+driver.get('https://books.toscrape.com')
