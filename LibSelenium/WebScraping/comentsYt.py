614dfa867ef01f715a99ce4dbdec4d4f8e8c01c2
Elorriaga <pedro.elorriaga@accenture.com>
2023-12-22 16:39:17 -0300

    Envio 22/12/23

--- LibSelenium/WebScraping/comentsYt.py
+++ LibSelenium/WebScraping/comentsYt.py
@@ -2,29 +2,29 @@ import pandas as pd
 import pathlib
 import time
 
+from colorama import Fore
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option('detach', True)
-
+options.add_argument('--start-maximized')
 driver = webdriver.Chrome(options=options)
-
-driver.get('https://www.youtube.com/watch?v=gxLY8u3Cgxs')
-
-
-driver.maximize_window()
-time.sleep(4)
+driver.get('https://www.youtube.com/watch?v=DLMCd4f23_M&ab_channel=SOLUCIONANDOFILMES')
 
 driver.execute_script('window.scrollBy(0, 600);')
-time.sleep(2)
+time.sleep(4)
 
 # CONTADOR DE COMENTARIOS
-count_section = driver.find_element(By.CLASS_NAME, 'count-text')
-count_int = int(count_section.find_elements(By.TAG_NAME, 'span')[0].text)
+print(Fore.GREEN + 'Iniciando o programa' + Fore.RESET)
+count_section = WebDriverWait(driver, 20).until(
+    EC.presence_of_element_located((By.CLASS_NAME, 'count-text'))
+)
+count_int = int(count_section.find_elements(By.TAG_NAME, 'span')[0].text.replace(',', ''))
 
+print(Fore.CYAN + 'Contando os comentarios, AGUARDE!!' + Fore.RESET)
 for i in range (0, count_int, 10):
     time.sleep(2)
     driver.execute_script('window.scrollBy(0, 1000);')
@@ -34,14 +34,18 @@ comments_section = WebDriverWait(driver, 10).until(
     )
 
 # COMENTARIOS
+print(Fore.RED + 'Gerando arquivo...' + Fore.RESET)
 comments = comments_section.find_elements(By.CLASS_NAME, 'ytd-item-section-renderer')
 
 comments_texts = []
 for comment in comments:
     autor = comment.find_element(By.TAG_NAME, 'yt-formatted-string')
     comments_text = comment.find_element(By.ID, 'content-text')
-    comments_texts.append({'Autor: ' : autor.text, 'Comentario: ' : comments_text.text})
+    comments_texts.append({'Autor' : autor.text, 'Comentario' : comments_text.text})
 
 file_path = pathlib.Path(__file__).parent
 df = pd.DataFrame(comments_texts)
-data_pandas = df.to_csv('teste.txt')
+
+# foi efetuado 
+data_pandas = df.to_excel(str(count_int) + 'comentarios.xlsx')
+print(Fore.GREEN + 'Concluido com sucesso!!' + Fore.RESET)
