26c3106f8e898e63a24bfbd070fada61af722ae5
Pedro Henrique Elorriaga dos Santos <pedro.elorriaga@gmvc.local>
2024-01-17 21:21:38 -0300

    Envio 17/01/24

--- FastAPI/Secao-01/main.py
+++ FastAPI/Secao-01/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Response, Path
+from pydantic import ValidationError
+from models import Curso
 
 app = FastAPI()
 
@@ -22,16 +24,51 @@ async def get_cursos() -> dict:
     return {'cursos disponiveis': cursos}
 
 
-@app.get('/cursos/{curso_id}')
-async def get_curso(curso_id: int):
+@app.get('/cursos/{id_key}')
+async def get_curso(id_key: int):
     try:
-        curso: dict = cursos[curso_id]
+        curso: dict = cursos[id_key]
+        return curso
     except KeyError:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
 
-    return curso
 
+@app.get('/cursos/limite/{id_key}')
+async def get_limited_curso(id_key: int = Path(title='ID do curso', description='O ID precisa ser entre 1 e 2', ge=1, le=2)):
+    try:
+        curso: dict = cursos[id_key]
+        return curso
+    except KeyError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
+
+
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    id_key: int = len(cursos) + 1
+    cursos[id_key] = curso
+    return {'msg': 'Curso criado com sucesso'}
+
+
+@app.put('/cursos/{id_key}')
+async def put_curso(id_key: int, curso: Curso):
+    if id_key in cursos:
+        cursos[id_key] = curso
+        return {'msg': {f'Alteração no ID {id_key} efetuado com sucesso!'}}
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Não existe um curso com o ID {id_key}')
+
+
+@app.delete('/cursos/{id_key}')
+async def delete_curso(id_key: int):
+    if id_key in cursos:
+        del cursos[id_key]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'O curso com ID {id_key} não existe!')
 
 if __name__ == '__main__':
     import uvicorn
--- FastAPI/Secao-01/models.py
+++ FastAPI/Secao-01/models.py
@@ -3,7 +3,6 @@ from pydantic import BaseModel
 
 
 class Curso(BaseModel):
-    id: Optional[int] = None
     titulo: str
     aulas: int
     horas: int
