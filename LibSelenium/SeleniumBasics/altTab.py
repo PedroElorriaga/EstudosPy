bb96a6550fc208b5c1e6b48eed245f02b576e5e0
Pedro Henrique Elorriaga dos Santos <pedro.elorriaga@gmvc.local>
2024-01-19 09:09:29 -0300

    Envio 18/01/24

--- LibSelenium/SeleniumBasics/altTab.py
+++ LibSelenium/SeleniumBasics/altTab.py
@@ -3,7 +3,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-options = webdriver.ChromeOptions() # INSTANCIANDO CHROMEDRIVER
+options = webdriver.ChromeOptions()  # INSTANCIANDO CHROMEDRIVER
+
 
 def make_chrome_browse(*optionsParam):
     if optionsParam is not None:
@@ -11,14 +12,15 @@ def make_chrome_browse(*optionsParam):
             options.add_argument(option)
 
     driver = webdriver.Chrome(
-        options=options, # UTILIZANDO AS OPÇÕES DO CRHOME
+        options=options,  # UTILIZANDO AS OPÇÕES DO CRHOME
     )
 
     return driver
 
+
 if __name__ == '__main__':
 
-    optionsTuple = '--disable-gpu', '--start-maximized', # CHROME OPTIONS
+    optionsTuple = '--disable-gpu', '--start-maximized',  # CHROME OPTIONS
     browser = make_chrome_browse(*optionsTuple)
 
     browser.get('https://www.google.com.br')
@@ -27,4 +29,5 @@ if __name__ == '__main__':
     browser.switch_to.window(browser.window_handles[0])
     time.sleep(2)
     browser.switch_to.window(browser.window_handles[1])
+    browser.close()
     time.sleep(10)
