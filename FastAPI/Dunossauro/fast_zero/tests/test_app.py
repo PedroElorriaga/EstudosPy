6c26fb0f659f1136f0eb8e444c7beaac3909bd9f
PedroElorriaga <pedrohes2002@outlook.com>
2024-02-28 22:17:15 -0300

    Envio 28/02/24

---
+++ FastAPI/Dunossauro/fast_zero/tests/test_app.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_root_retornar_using_fastapi_e_retornar_status_200():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/')  # Act
+
+    assert response.status_code == 200  # Assert
+    assert response.json() == {'message': 'Using FastAPI'}  # Assert
