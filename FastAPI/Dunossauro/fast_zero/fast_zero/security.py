0bff48df24023dc4555ad229f76269668855c640
PedroElorriaga <pedrohes2002@outlook.com>
2024-03-27 23:10:26 -0300

    Configuração de JWT para implementação

---
+++ FastAPI/Dunossauro/fast_zero/fast_zero/security.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+
+from jwt import encode
+from passlib.context import CryptContext
+
+SECRET_KEY = 'chave-secreta'
+ALGORITHM = 'HS256'
+ACESS_TOKEN_EXPIRE_MINUTES = 30
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+def criar_token_de_acesso(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({'exp': expire})
+    encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
