a984de85fff16b082db46d0f576cfa66127e4433
PedroElorriaga <pedrohes2002@outlook.com>
2024-04-16 22:53:05 -0300

    Envio 16/04/24

--- FastAPI/Dunossauro/fast_zero/fast_zero/app.py
+++ FastAPI/Dunossauro/fast_zero/fast_zero/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.routes import auth, usuarios
 
@@ -7,3 +8,18 @@ app = FastAPI()
 
 app.include_router(usuarios.router)
 app.include_router(auth.router)
+
+
+@app.get('/', response_class=HTMLResponse)
+def home_page():
+    return """
+    <html>
+        <head>
+            <title>Home Page</title>
+        </head>
+
+        <body style="font-family: Arial;">
+            <h1 style="margin-left: 5%;">Use o <strong>/docs</strong> para vizualizar as funcionalidades</h1>
+        </body>
+    </html>
+    """
