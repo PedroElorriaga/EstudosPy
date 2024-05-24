8817e40e8cb7fa8165015af9f27b4fbc65efc9d4
PedroElorriaga <pedrohes2002@outlook.com>
2024-05-24 09:14:49 -0300

    Envio 24/05/24: Corrigido a falta de dependência em routes/phones

--- FastAPI/Alugaphone/aluga-project/aluga_project/routes/phones.py
+++ FastAPI/Alugaphone/aluga-project/aluga_project/routes/phones.py
@@ -1,19 +1,23 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 from sqlalchemy import select
+from sqlalchemy.orm import Session
 
 from aluga_project.database.database import create_session
 from aluga_project.models.models import PhoneStock
 from aluga_project.schemas.schema import PhonesList
 
+Session = Annotated[Session, Depends(create_session)]
+
 router = APIRouter(prefix='/phones', tags=['phones'])
 
 
 @router.get('/', status_code=200, response_model=PhonesList)
-def phone_get(session: create_session, skip: int = 0, limit: int = 100):
+def phone_get(session: Session, skip: int = 0, limit: int = 100):
 
-    print('TESTE')
     phones_list_from_db = session.scalars(
-        select(PhoneStock).offset(skip).limit(limit).all()
-    )
+        select(PhoneStock).offset(skip).limit(limit)
+    ).all()
 
     return {'phones': phones_list_from_db}
